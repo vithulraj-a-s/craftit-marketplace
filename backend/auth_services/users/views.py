e73aeac0750c60e40aa205a922a2da6a38b01fd6
vithul <vithulrajas@gmail.com>
2026-03-14 14:33:34 +0530

    added user registration API using serializers and routes

--- backend/auth_services/users/views.py
+++ backend/auth_services/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+class registerView(generics.CreateAPIView):
+    queryset = User.objects.all()
+
+    serializer_class = RegisterSerializer
