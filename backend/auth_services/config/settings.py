1a5e1808e8d14c16c82390af12bd92c009b6be2f
vithul <vithulrajas@gmail.com>
2026-03-14 10:18:48 +0530

    Implemented custom user model with AbstractBaseUser

--- backend/auth_services/config/settings.py
+++ backend/auth_services/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +79,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST',default='localhost'),
+        'PORT': config('DB_PORT',default='5432'),
     }
 }
+
+# custom user model created using AbstractBaseuser
+AUTH_USER_MODEL = "users.User"
 
 
 # Password validation
--- backend/auth_services/users/models.py
+++ backend/auth_services/users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
 
 # Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin):
+    ROLE_CHOICES = (
+        ("ARTIST","Artist"),
+        ("CLIENT","Client"),
+        ("ADMIN","Admin"),
+    )
+
+    email = models.EmailField(unique=True)
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    is_verified = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+    
+    
